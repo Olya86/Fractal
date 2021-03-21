ebe6657180c651d145a4636155a0d441ea0a51ac
Olya <Olya@GO>
2021-03-21 09:34:59 +0300

    add

--- Form1.cs
+++ Form1.cs
@@ -22,10 +22,43 @@ namespace Fractal2
             System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
             System.Drawing.Graphics formGraphics;
             formGraphics = this.CreateGraphics();
-            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+            //formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+           
+
+            //Point point1 = new Point(10, 10);
+            //Point point2 = Point.Add(point1, new Size(0, 46));
+            //formGraphics.DrawLine(Pens.Red, point1, point2);
+
+            formGraphics.FillEllipse(Brushes.Green, new Rectangle(300, 50, 5, 5));
+            formGraphics.FillEllipse(Brushes.Green, new Rectangle(150, 300, 5, 5));
+            formGraphics.FillEllipse(Brushes.Green, new Rectangle(450, 300, 5, 5));
+
+            Random rnd = new Random();
+
+            //Получить случайное число (в диапазоне от 0 до 10)
+            int x1 = rnd.Next(1, 3);
+            formGraphics.FillEllipse(Brushes.Green, new Rectangle(450, 300, 5, 5));
             myBrush.Dispose();
             formGraphics.Dispose();
         }
+        public class Point
+        {
+            private decimal _x;
+            private decimal _y;
+
+            public decimal X
+            {
+              get { return _x; }
+              set { _x = value; }
+            }
+
+            public decimal Y
+            {
+                get { return _y; }
+                set { _y = value; }
+            }
+        }
+
 
 
     }
